cb1e7a1faac7c699a2afab3126bc0340f2a46256
Maira Bello <maira.araujo@liferay.com>
2015-02-03 14:40:40 -0300

    Adds dist to the repo for now

---
+++ dist/modal.js
@@ -0,0 +1,62 @@
+import templates from 'alloyui-modal/modal.soy';
+import Tooltip from 'alloyui-tooltip';
+
+function Modal(opt_config) {
+  Modal.base(this, 'constructor', opt_config);
+}
+lfr.inherits(Modal, lfr.SoyComponent);
+
+Modal.ATTRS = {
+  bodyContent: {
+    value: ''
+  },
+  elementClasses: {
+    value: ['modal']
+  },
+  footerButtons: {
+    valueFn: function() {
+      return [];
+    }
+  },
+  headerContent: {
+    value: ''
+  },
+  visible: {
+    value: false
+  }
+};
+
+Modal.ATTRS_SYNC = ['visible'];
+
+Modal.SURFACES = {
+  body: {
+    renderAttrs: ['bodyContent']
+  },
+  footer: {
+    renderAttrs: ['footerButtons']
+  },
+  header: {
+    renderAttrs: ['headerContent']
+  }
+};
+
+Modal.TEMPLATES = templates;
+
+Modal.prototype.attached = function() {
+  var instance = this;
+
+  this.delegate('click', '.modal-button', function(event) {
+    instance.emit('buttonClicked', {button: event.delegateTarget});
+  });
+
+  this.tooltip_ = new Tooltip({
+    content: 'Modal',
+    trigger: this.element.querySelector('.modal-header')
+  }).render();
+};
+
+Modal.prototype.syncVisible = function() {
+  this.element.style.display = this.visible ? 'block' : 'none';
+};
+
+export default Modal;
---
+++ dist/modal.soy.js
@@ -0,0 +1,72 @@
+// This file was automatically generated from modal.soy.
+// Please don't edit this file by hand.
+
+/**
+ * @fileoverview Templates in namespace templates.
+ */
+
+if (typeof templates == 'undefined') { var templates = {}; }
+
+
+/**
+ * @param {Object.<string, *>=} opt_data
+ * @param {(null|undefined)=} opt_ignored
+ * @return {!soydata.SanitizedHtml}
+ * @suppress {checkTypes}
+ */
+templates.element = function(opt_data, opt_ignored) {
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml('<div class="modal-dialog"><div class="modal-content"><div id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '-header" class="modal-header"></div><div id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '-body" class="modal-body"></div><div id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '-footer" class="modal-footer"></div></div></div>');
+};
+if (goog.DEBUG) {
+  templates.element.soyTemplateName = 'templates.element';
+}
+
+
+/**
+ * @param {Object.<string, *>=} opt_data
+ * @param {(null|undefined)=} opt_ignored
+ * @return {!soydata.SanitizedHtml}
+ * @suppress {checkTypes}
+ */
+templates.body = function(opt_data, opt_ignored) {
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml('<p>' + soy.$$escapeHtml(opt_data.bodyContent) + '</p>');
+};
+if (goog.DEBUG) {
+  templates.body.soyTemplateName = 'templates.body';
+}
+
+
+/**
+ * @param {Object.<string, *>=} opt_data
+ * @param {(null|undefined)=} opt_ignored
+ * @return {!soydata.SanitizedHtml}
+ * @suppress {checkTypes}
+ */
+templates.header = function(opt_data, opt_ignored) {
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml('<h4 class="modal-title">' + soy.$$escapeHtml(opt_data.headerContent) + '</h4>');
+};
+if (goog.DEBUG) {
+  templates.header.soyTemplateName = 'templates.header';
+}
+
+
+/**
+ * @param {Object.<string, *>=} opt_data
+ * @param {(null|undefined)=} opt_ignored
+ * @return {!soydata.SanitizedHtml}
+ * @suppress {checkTypes}
+ */
+templates.footer = function(opt_data, opt_ignored) {
+  var output = '';
+  var buttonList19 = opt_data.footerButtons;
+  var buttonListLen19 = buttonList19.length;
+  for (var buttonIndex19 = 0; buttonIndex19 < buttonListLen19; buttonIndex19++) {
+    var buttonData19 = buttonList19[buttonIndex19];
+    output += '<button type="button" class="modal-button ' + soy.$$escapeHtmlAttribute(buttonData19['class']) + '">' + soy.$$escapeHtml(buttonData19.label) + '</button>';
+  }
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml(output);
+};
+if (goog.DEBUG) {
+  templates.footer.soyTemplateName = 'templates.footer';
+}
+export default templates;
