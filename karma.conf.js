d2933fa8b8a590d956f11d6abadcb6f174c583b0
Eduardo Lundgren <eduardolundgren@gmail.com>
2015-03-04 11:45:56 -0300

    Updates karma processor to use babel

--- karma.conf.js
+++ karma.conf.js
@@ -16,19 +16,19 @@ module.exports = function(config) {
 
     preprocessors: {
       // All src files should be included in the coverage report. These
-      // files don't need to go through the `6to5` preprocessor, as the
+      // files don't need to go through the `babel` preprocessor, as the
       // `coverage` preprocessor already does the necessary conversion.
       'src/**/!(*.soy).js': ['coverage'],
 
       // Since tests, soy files and jspm packages are not going through
       // the `coverage` preprocessor we need to explicitly make them go
-      // through `6to5`.
-      'src/**/*.soy.js': ['6to5'],
-      'test/**/*.js': ['6to5'],
-      'jspm_packages/?*/**/*.js': ['6to5']
+      // through `babel`.
+      'src/**/*.soy.js': ['babel'],
+      'test/**/*.js': ['babel'],
+      'jspm_packages/?*/**/*.js': ['babel']
     },
 
-    '6to5Preprocessor': {
+    'babelPreprocessor': {
       options: {
         sourceMap: true,
         modules: 'system'
